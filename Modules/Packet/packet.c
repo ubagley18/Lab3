315d0f373104eeadf4b2a18f9d3293633f86a485
Prashant Shrestha <sthprashant@gmail.com>
2020-04-01 20:12:33 +1100

    corrected variable name and fixed a few other build errors and warnings + spacing, styling and comments fixed

--- Modules/Packet/packet.c
+++ Modules/Packet/packet.c
@@ -10,7 +10,7 @@
 
 // New types
 #include "packet.h"
-//#include "UART\UART.h"
+#include "UART\UART.h"
 
 //
 
@@ -21,7 +21,7 @@ uint8_t Packet_Command,    /*!< The packet's command */
 		Packet_Parameter3, /*!< The packet's 3rd parameter */
 		Packet_Checksum;   /*!< The packet's checksum */
 
-static uint8_t currentState = 0; // current state
+static uint8_t currentState = 0; // current state of FSM
 
 
 
@@ -39,34 +39,34 @@ bool Packet_Get(void){
 	switch(currentState){
 	case 0:
 		if(UART_InChar(&Packet_Command)) // true if packet received from RxFIFO
-			state = 1; // currentState value will be changed to 1 if true
+			currentState = 1; // currentState value will be changed to 1 if true
 		else
 			return false;
 		break;
 	case 1:
 		if(UART_InChar(&Packet_Parameter1))  // true if packet received from RxFIFO
-			state = 2; //currentState value will be changed to 2 if true
+			currentState = 2; //currentState value will be changed to 2 if true
 		else
 			return false;
 		break;
 	case 2:
-			if(UART_InChar(&Packet_Parameter2))  // true if packet received from RxFIFO
-				state = 3; //currentState value will be changed to 3 if true
-			else
-				return false;
-			break;
+		if(UART_InChar(&Packet_Parameter2))  // true if packet received from RxFIFO
+			currentState  = 3; //currentState value will be changed to 3 if true
+		else
+			return false;
+		break;
 	case 3:
-			if(UART_InChar(&Packet_Parameter3))  // true if packet received from RxFIFO
-				state = 4; //currentState value will be changed to 4 if true
-			else
-				return false;
-			break;
+		if(UART_InChar(&Packet_Parameter3))  // true if packet received from RxFIFO
+			currentState  = 4; //currentState value will be changed to 4 if true
+		else
+			return false;
+		break;
 	case 4:
-			if(UART_InChar(&Packet_Checksum))  // true if packet received from RxFIFO
-				state = 5; //currentState value will be changed to 5 if true
-			else
-				return false;
-			break;
+		if(UART_InChar(&Packet_Checksum))  // true if packet received from RxFIFO
+			currentState  = 5; //currentState value will be changed to 5 if true
+		else
+			return false;
+		break;
 	case 5:
 		//checking packet validity
 		if((Packet_Command ^ Packet_Parameter1 ^ Packet_Parameter2 ^ Packet_Parameter3) == Packet_Checksum){
@@ -74,22 +74,29 @@ bool Packet_Get(void){
 			return true; // packet received
 		}
 		else{
-			state = 4; // go to state 4 if packet not valid
+			//shift packets
+			Packet_Command = Packet_Parameter1; // Checksum does not add up, right shift bytes
+			Packet_Parameter1 = Packet_Parameter2;
+			Packet_Parameter2 = Packet_Parameter3;
+			Packet_Parameter3 = Packet_Checksum;
+			currentState = 4; // go to state 4 if packet not valid and look for another one
 			return false;
 		}
+		break;
 
 	}
+	return true;
 }
 
 
 bool Packet_Put(const uint8_t command, const uint8_t parameter1, const uint8_t parameter2, const uint8_t parameter3){
 
-	//packets sent byte by byte to TxFIFO
-	UART_OutChar(command);
-	UART_OutChar(parameter1);
-	UART_OutChar(parameter2);
-	UART_OutChar(parameter3);
-	UART_OutChar(command ^ parameter1 ^ parameter2 ^ parameter3 ); // value of checksum XORed before sending data
-
+		//packets sent byte by byte to TxFIFO
+		UART_OutChar(command);
+		UART_OutChar(parameter1);
+		UART_OutChar(parameter2);
+		UART_OutChar(parameter3);
+		UART_OutChar(command ^ parameter1 ^ parameter2 ^ parameter3 ); // value of checksum XORed before sending data
+	return true;
 }
 
